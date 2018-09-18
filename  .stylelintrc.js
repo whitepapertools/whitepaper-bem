1454867e07372d3b73ab08acf3314bbdc8f9ae99
Никита Высотский <vysotskii@yamoney.ru>
2018-09-18 18:04:58 +0300

    linting

---  .stylelintrc.js
+++  .stylelintrc.js
@@ -1,7 +1,9 @@
-{
+module.exports = {
  "extends": [
   "stylelint-config-standard"],
   "rules": {
+    "color-no-invalid-hex": true,
+    "color-hex-case": "lower",
     "indentation": "tab"
   }
 }
